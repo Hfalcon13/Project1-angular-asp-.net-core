8fcb1fcae18bd72ff7f052e2adef98052f3b4ece
harel <harelyishay@gmail.com>
2023-04-09 14:13:57 +0300

    Add project files.

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Project1_angular___asp_.net_core
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
